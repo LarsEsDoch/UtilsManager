743ee46ccb9a6f542ad8c8696a2beaaa5b72ec6a
Lars <lars.bogatz@gmail.com>
2025-11-02 12:02:59 +0100

    Removed unused code

--- src/main/java/de/lars/utilsmanager/util/Statements.java
+++ src/main/java/de/lars/utilsmanager/util/Statements.java
@@ -23,35 +23,7 @@ public class Statements {
         }
     }
 
-    public static Component getPlayerNotFound(Player player, String name) {
-        if (LanguageAPI.getApi().getLanguage(player) == 2)
-            return Component.text("Spieler " + name + " nicht gefunden.", NamedTextColor.RED);
-        else {
-            return Component.text("Player " + name + " not found.", NamedTextColor.RED);
-        }
-    }
-
     public static Component getOnlyPlayers() {
         return Component.text("Only player can send Messages.", NamedTextColor.RED);
-    }
-
-    public static Component formatDuration(long totalSeconds) {
-        if (totalSeconds < 0) totalSeconds = 0;
-
-        int days = Math.toIntExact(totalSeconds / 86400);
-        int hours = Math.toIntExact((totalSeconds / 3600) % 24);
-        int minutes = Math.toIntExact((totalSeconds / 60) % 60);
-        int seconds = Math.toIntExact(totalSeconds % 60);
-
-        String formatted;
-        if (days > 0) {
-            formatted = String.format("%02dd %02dh %02dm %02ds", days, hours, minutes, seconds);
-        } else if (hours > 0) {
-            formatted = String.format("%02dh %02dm %02ds", hours, minutes, seconds);
-        } else {
-            formatted = String.format("%02dm %02ds", minutes, seconds);
-        }
-
-        return Component.text(formatted, NamedTextColor.GOLD);
     }
 }
